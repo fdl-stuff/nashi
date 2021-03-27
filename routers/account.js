d53644fdddd59ba7154ab54d38fed54bf772cc33
twentythreetea <helimc.minecraft@gmail.com>
2021-03-27 06:12:03 +0000

    1.0.1:
    a lot of modifications to the login page and I also made cookie-notice shows up until you decline it.
    
    TODO: make /konto/forgot for forgotten passwords, I should do this alongside of the formular cause both need email.

--- routers/account.js
+++ routers/account.js
@@ -1,39 +1,39 @@
 const {express, String, argon2} = require('../init');
 const mysql = require('utility/mysql');
+const login_errors = require("../constants/login_errors");
 const router = express.Router();
 
 router.get('/login', (req, res, next) => {
-    //add remember me and password forgotten
+    req.data.login_error = req.query.error ? login_errors[req.query.error] : {};
     req.data.type = "login";
     req.data.page_title = "Login"
     if(req.data.user.id) return res.redirect('/konto/logout?redir='+req.data.redir);
     res.render('pages/index', req.data);
 });
 router.post('/login', (req, res, next) => {
-    if(!req.body || !req.body.email || !req.body.password) return res.redirect('/login');
-    if(String.isPasswordValidator(req.body.password)) {
+    if(!req.body) return res.redirect(`/konto/login?redir=${req.data.redir}`);
+    try {
+        String.isPassword(req.body.password);
+        String.isEmail(req.body.email);
         mysql.$query(`SELECT password FROM users WHERE email = ?`, [req.body.email], {
             req, res, next, async handler(error, result, fields, router) {
                 if(error) return router.next(new errorHandling.SutekinaError(error.message, 400));
-                //if the email is invalid
-                if(!result[0]) return res.redirect('/konto/login')
+                if(!result[0]) return res.redirect(`/konto/login?error=LOGIN_FAILED&redir=${req.data.redir}`)
                 try {
                     if(await argon2.verify(result[0].password, req.body.password)) {
                         req.session.email = req.body.email;
-                        res.redirect(req.data.redir);
+                        return res.redirect(req.data.redir);
                     } else {
-                        //wrong password
-                        return res.redirect('/konto/login')
+                        return res.redirect(`/konto/login?error=LOGIN_FAILED&redir=${req.data.redir}`)
                     }
                 } catch(err) {
-                    //internal failure
-                    next(new errorHandling.SutekinaError(err.message, 500));
+                    return next(new errorHandling.SutekinaError(err.message, 500));
                 }
             }
-
         });
-        //if the password isnt a valid password (too short too long yea :D idk can happen if someone requests manually this isnbt perfect cause someone could set a short password in hte database and then would be unable to login hm id change the minimum to 1 in the utility config for strings)
-    } else return res.redirect('/konto/login');
+    } catch(err) {
+        return res.redirect(`/konto/login?error=${err.message}&redir=${req.data.redir}`);
+    }
 });
 router.get('/logout', (req, res, next) => {
     req.session.destroy((err) => {
